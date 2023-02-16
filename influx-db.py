0d2a0ee9512c496a9e55109c690cac059892e2e4
DMNGS <samuel.dmngs@eduge.ch>
2023-02-16 12:43:19 +0100

    Updated InfluxDB connector script

--- influx-db.py
+++ influx-db.py
@@ -1,5 +1,48 @@
+# Script      : dbcon.py
+# Description : databse connector
+# Author      : DOMINGUES PEDROSA Samuel
+# Date        : 2023.02.16, V1.0
 from influxdb import InfluxDBClient
 
-client = InfluxDBClient(host='sdmansion.local', port=8086, username='patchouli', password='koakoakuma', ssl=True, verify_ssl=True)
+class DbChef:
+    """
+    A MariaDB databse connector
 
-print(client.get_list_database())
+    Attributes
+    ----------
+    client : InfluxDBClient
+        the connector to the databse
+
+    Methods
+    -------
+    select(table)
+        Get all values from table
+    """
+
+    def __init__(self):
+        load_dotenv()
+        host = os.getenv('DB_HOST')
+        port = os.getenv('DB_PORT')
+        user = os.getenv('DB_USER')
+        password = os.getenv('DB_PASSWORD')
+        database = os.getenv('DB_DATABASE')
+        self.client = InfluxDBClient(host, port, user, password, database)
+    pass
+
+    def select(table):
+        """
+        Get all values from table
+
+        Parameters
+        ----------
+        table: str
+            the table
+        Returns
+        -------
+        ResultSet
+            the result of the query
+        """
+        data = self.client.query(f'SELECT * FROM {table}')
+        return data
+
+client = InfluxDBClient('testbench.local', 8086, 'graphana', 'lmp2020', 'home')
--- mqtt-sub.py
+++ mqtt-sub.py
@@ -3,18 +3,18 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Connected to MQTT Broker!")
+        print('Connected to MQTT Broker!')
     else:
-        print("Failed to connect, return code %d\n", rc)
+        print('Failed to connect, return code %d\n', rc)
 
 
 def on_message(client, userdata, message):
-    print("recived message: ", str(message.payload.decode("utf-8")))
+    print('recived message: ', str(message.payload.decode('utf-8')))
     
-mqttBroker = "10.5.46.52"
+mqttBroker = 'sdmansion.local'
 
-client = mqtt.Client("Server")
-#client.username_pw_set(username="buffet", password="tastybuffet")
+client = mqtt.Client('Server')
+client.username_pw_set(username='buffet', password='tastybuffet')
 client.on_connect = on_connect
 client.on_message = on_message
 
@@ -22,11 +22,11 @@ client.connect(mqttBroker)
 
 client.loop_start()
 
-client.subscribe("scale/value")
+client.subscribe('scale/value')
 
 try:
     while True:
         pass
 except:
-    print("Stopping")
+    print('Stopping')
     client.loop_stop()
